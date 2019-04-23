c22a972e5eb18fc9f4b72712ab994491667febb5
zdg <zhu527625750@163.com>
2019-04-23 20:20:51 +0800

    1、详细信息界面，更换了显示放大图片的库，该版本是用的时候photoView库

--- app/src/main/java/comvoice/example/zhangbin/startimage/activity/PreviewActivity.java
+++ app/src/main/java/comvoice/example/zhangbin/startimage/activity/PreviewActivity.java
@@ -5,13 +5,19 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.ImageView;
+
 import com.bumptech.glide.Glide;
+import com.github.chrisbanes.photoview.OnPhotoTapListener;
+import com.github.chrisbanes.photoview.PhotoView;
+
 import comvoice.example.zhangbin.startimage.R;
 import comvoice.example.zhangbin.startimage.view.ZoomImageView;
 
 public class PreviewActivity extends BaseActivity {
     Bitmap bp = null;
-    ZoomImageView imageview;//自定义imageview，可以随着手势放大或缩小
+//    ZoomImageView imageview;//自定义imageview，可以随着手势放大或缩小
+    private PhotoView photoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,15 +25,30 @@ public class PreviewActivity extends BaseActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON, WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);//禁止屏幕休眠
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_preview);
-        imageview = (ZoomImageView) findViewById(R.id.iv);
+//        imageview = (ZoomImageView) findViewById(R.id.iv);
+        photoView =findViewById(R.id.iv1);
         Intent intent = getIntent();
         String pathName = intent.getStringExtra("msg");
-        bp = BitmapFactory.decodeFile(pathName);//通过传递过来的路径转化为bitmap
+//        bp = BitmapFactory.decodeFile(pathName);//通过传递过来的路径转化为bitmap
+//        photoView.setEnabled(true);
+//        photoView.setImageBitmap(bp);
+
         Glide.with(getApplicationContext())//通过第三方框架展示传递过来的图片
                 .load(pathName)
-                .into(imageview);
-        imageview.setImage(bp);
+                .into(photoView);
+
+
+        photoView.setOnPhotoTapListener(new OnPhotoTapListener() {
+            @Override
+            public void onPhotoTap(ImageView view, float x, float y) {
+                finish();
+            }
+        });
     }
+
+
+
+
 
 
 
