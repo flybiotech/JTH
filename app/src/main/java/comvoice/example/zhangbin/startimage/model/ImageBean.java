fec0f41ac2e01034f3465ca649794b3a05fee46f
buxiaomaoCC <2398573148@qq.com>
2019-04-23 19:00:12 +0800

    选择框添加筛查ID；查看图像添加图片姓名；修复选择图片后记录未清除的bug

---
+++ app/src/main/java/comvoice/example/zhangbin/startimage/model/ImageBean.java
@@ -0,0 +1,22 @@
+package comvoice.example.zhangbin.startimage.model;
+
+public class ImageBean {
+    private String imageName;
+    private String imagePath;
+
+    public String getImageName() {
+        return imageName;
+    }
+
+    public void setImageName(String imageName) {
+        this.imageName = imageName;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+}
